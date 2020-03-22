7761fc7ce2d1b872456ea0cff421048fe59c4d71
Lloyd Headbush <ocanica@gmail.com>
2020-03-22 21:17:54 +0000

    Added async methods to MoviesController

---
+++ src/OMDbApi.Api/Controllers/MoviesController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMDbApi.Api.Controllers
+{
+    public class MoviesController
+    {
+    }
+}
